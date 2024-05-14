fdd578392749689f3fc60cae5ff4cba08db82161
win/ubuntu <d-brandenburg@hotmail.de>
2024-05-14 14:07:03 +0200

    login frontend erstellt.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,62 @@
-import { useEffect, useContext, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import Navbar from './components/Navbar/Navbar.jsx';
-import Footer from './components/Footer/Footer.jsx';
-import HomePage from './pages/HomePage/HomePage.jsx';
-import ErrorPage from './pages/ErrorPage/ErrorPage.jsx';
-import './styles/App.scss';
-import { ModalContext } from './contexts/ModalContext.js';
-import GamesPage from './pages/GamesPage/GamesPage.jsx';
+import { useEffect, useContext, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+import ErrorPage from "./pages/ErrorPage/ErrorPage.jsx";
+import "./styles/App.scss";
+import { ModalContext } from "./contexts/ModalContext.js";
+import GamesPage from "./pages/GamesPage/GamesPage.jsx";
 
 function App() {
+  const {
+    openModalBlocker,
+    setOpenModalBlocker,
+    openSearch,
+    setOpenSearch,
+    openCart,
+    setOpenCart,
+    openLoginModal,
+    setOpenLoginModal,
+  } = useContext(ModalContext);
 
-	const { openModalBlocker, setOpenModalBlocker, openSearch, setOpenSearch, openCart, setOpenCart } = useContext(ModalContext);
-
-	const handleModalClose = (e) => {
-		e.preventDefault();
-		if (openModalBlocker) {
-			setOpenModalBlocker(false)
-		}
-		if (openSearch) {
-			setOpenSearch(false)
-		}
-		if (openCart) {
-			setOpenCart(false)
-		}
-	}
+  const handleModalClose = (e) => {
+    e.preventDefault();
+    if (openModalBlocker) {
+      setOpenModalBlocker(false);
+    }
+    if (openSearch) {
+      setOpenSearch(false);
+    }
+    if (openCart) {
+      setOpenCart(false);
+    }
+    if (openLoginModal) {
+      setOpenLoginModal(false);
+    }
+  };
   return (
     <>
-			<Navbar />
-			<ToastContainer
-				autoClose={3000}
-				theme="dark"
-				pauseOnHover={false}
-				pauseOnFocusLoss={false}
-				newestOnTop
-			/>
-			<main>
-				<Routes>
-					<Route path='/' element={<HomePage /> } />
-					<Route path='/games' element={<GamesPage /> } />
-					<Route path='*' element={<ErrorPage /> } />
-				</Routes>
-			</main>
-			<Footer />
-			{openModalBlocker && <div className='modal-blocker' onClick={handleModalClose}></div>}
+      <Navbar />
+      <ToastContainer
+        autoClose={3000}
+        theme="dark"
+        pauseOnHover={false}
+        pauseOnFocusLoss={false}
+        newestOnTop
+      />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/games" element={<GamesPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
+      <Footer />
+      {openModalBlocker && (
+        <div className="modal-blocker" onClick={handleModalClose}></div>
+      )}
     </>
   );
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,102 +1,157 @@
-import React, { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
-import { ScreenModeContext } from '../../contexts/ScreenModeContext.js';
-import { ModalContext } from '../../contexts/ModalContext.js';
-import Logo from '../../assets/pixelPlaza.webp';
-import './navbar.scss';
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { ScreenModeContext } from "../../contexts/ScreenModeContext.js";
+import { ModalContext } from "../../contexts/ModalContext.js";
+import Logo from "../../assets/pixelPlaza.webp";
+import "./navbar.scss";
+import Login from "../Navbar/Login.jsx";
+import { set } from "date-fns";
 
 const Navbar = () => {
+  const {
+    openModalBlocker,
+    setOpenModalBlocker,
+    openSearch,
+    setOpenSearch,
+    openCart,
+    setOpenCart,
+    openLoginModal,
+    setOpenLoginModal,
+  } = useContext(ModalContext);
+  const { screenMode, setScreenMode } = useContext(ScreenModeContext);
 
-	const { openModalBlocker, setOpenModalBlocker, openSearch, setOpenSearch, openCart, setOpenCart, openLoginModal,setOpenLoginModal } = useContext(ModalContext);
-	const { screenMode, setScreenMode } = useContext(ScreenModeContext);
+  const cartItems = ["Assassins Creed", "Cyberpunk"];
+  const cIL = cartItems.length;
 
-	const cartItems = ["Assassins Creed", "Cyberpunk"];
-	const cIL = cartItems.length;
+  const handleOpenSearch = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (openCart && openModalBlocker) {
+      setOpenCart(false);
+    } else {
+      setOpenModalBlocker((prev) => !prev);
+    }
+    setOpenSearch((prev) => !prev);
+  };
 
-	const handleOpenSearch = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-		if (openCart && openModalBlocker) {
-			setOpenCart(false)
-		} else {
-			setOpenModalBlocker(prev => !prev);
-		}
-		setOpenSearch(prev => !prev);
-	};
+  const handleCloseAll = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setOpenModalBlocker(false);
+    setOpenSearch(false);
+    setOpenCart(false);
+    setOpenLoginModal(false);
+  };
 
-	const handleCloseAll = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-		setOpenModalBlocker(false);
-		setOpenSearch(false);
-		setOpenCart(false);
-	}
-	
-	const handleOpenCart = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-		if (openSearch && openModalBlocker) {
-			setOpenSearch(false)
-		} else {
-			setOpenModalBlocker(prev => !prev)
-		}
-		setOpenCart(prev => !prev)
-	};
+  const handleOpenCart = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (openSearch && openModalBlocker) {
+      setOpenSearch(false);
+    } else {
+      setOpenModalBlocker((prev) => !prev);
+    }
+    setOpenCart((prev) => !prev);
+  };
 
-	const handleOpenLogin = (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-		setOpenModalBlocker(prev => !prev);
-	};
+  const handleOpenLogin = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if ((openSearch || openCart) && openModalBlocker) {
+      setOpenSearch(false);
+      setOpenCart(false);
+    } else {
+      setOpenModalBlocker((prev) => !prev);
+    }
+    setOpenLoginModal((prev) => !prev);
+  };
 
-	const handleScreenMode = (e) => {
-		e.preventDefault();
-		if (screenMode === 'light') {
-			setScreenMode('dark')
-			localStorage.setItem("screenMode", "dark")
-		} else {
-			setScreenMode('light')
-			localStorage.setItem("screenMode", "light")
-		}
-	};
+  const handleScreenMode = (e) => {
+    e.preventDefault();
+    if (screenMode === "light") {
+      setScreenMode("dark");
+      localStorage.setItem("screenMode", "dark");
+    } else {
+      setScreenMode("light");
+      localStorage.setItem("screenMode", "light");
+    }
+  };
 
-	return (
-		<nav>
-			<div className='nav-wrapper' onClick={handleCloseAll}>
-				<div className='nav-left'>
-					<NavLink to='/' className='logo-wrapper'>
-						<img src={Logo} alt="logo" />
-					</NavLink>
-					{!openSearch && <div className='item-wrapper'>
-						<NavLink to='/games' className='nav-link' title='navlink'>Spiele</NavLink>
-						<NavLink to='/infos' className='nav-link' title='navlink'>Infos</NavLink>
-						<NavLink to='/forum' className='nav-link' title='navlink'>Forum</NavLink>
-						<NavLink to='/help' className='nav-link' title='navlink'>Hilfe</NavLink>
-						<p className='nav-link' onClick={handleOpenLogin}>Einloggen</p>
-					</div>}
-				</div>
-				{openSearch && <div className='nav-mid'>
-					<label htmlFor="search"><i className='bi bi-search'></i></label>
-					<input type="text" className='nav-search' name="search" id="search" autoFocus onClick={((e) => e.stopPropagation())}/>
-				</div>}
-				<div className="nav-right">
-					<div className='search-wrapper' onClick={handleOpenSearch}>
-						<i className={`bi ${!openSearch ? 'bi-search' : 'bi-x-lg'}`}></i>
-					</div>
-					<div className='cart-wrapper' onClick={handleOpenCart}>
-						<i className='bi bi-cart4' ></i>
-						<small className={`cart-count ${cIL > 0 ? 'cart-items' : 'cart-empty'}`} style={{color: cIL > 0 ? '#fff' : 'unset', background: cIL > 0 ? '#f85525' : 'unset'}}>{cIL}</small> {/* Cart Count */}
-						{openCart && <div className='cart-modal' onClick={((e) => e.stopPropagation())}>
-							<p>Cart Modal</p>
-						</div>}
-					</div>
-					<div className='screenmode-wrapper' onClick={handleScreenMode}>
-						{screenMode === 'light' ? <i className="bi bi-moon"></i> : <i className="bi bi-brightness-high"></i>}
-					</div>
-				</div>
-			</div>
-		</nav>
-	)
-}
+  return (
+    <nav>
+      <div className="nav-wrapper" onClick={handleCloseAll}>
+        <div className="nav-left">
+          <NavLink to="/" className="logo-wrapper">
+            <img src={Logo} alt="logo" />
+          </NavLink>
+          {!openSearch && (
+            <div className="item-wrapper">
+              <NavLink to="/games" className="nav-link" title="navlink">
+                Spiele
+              </NavLink>
+              <NavLink to="/infos" className="nav-link" title="navlink">
+                Infos
+              </NavLink>
+              <NavLink to="/forum" className="nav-link" title="navlink">
+                Forum
+              </NavLink>
+              <NavLink to="/help" className="nav-link" title="navlink">
+                Hilfe
+              </NavLink>
+              <p className="nav-link" onClick={handleOpenLogin}>
+                Einloggen
+                {openLoginModal && <Login />}
+              </p>
+            </div>
+          )}
+        </div>
+        {openSearch && (
+          <div className="nav-mid">
+            <label htmlFor="search">
+              <i className="bi bi-search"></i>
+            </label>
+            <input
+              type="text"
+              className="nav-search"
+              name="search"
+              id="search"
+              autoFocus
+              onClick={(e) => e.stopPropagation()}
+            />
+          </div>
+        )}
+        <div className="nav-right">
+          <div className="search-wrapper" onClick={handleOpenSearch}>
+            <i className={`bi ${!openSearch ? "bi-search" : "bi-x-lg"}`}></i>
+          </div>
+          <div className="cart-wrapper" onClick={handleOpenCart}>
+            <i className="bi bi-cart4"></i>
+            <small
+              className={`cart-count ${cIL > 0 ? "cart-items" : "cart-empty"}`}
+              style={{
+                color: cIL > 0 ? "#fff" : "unset",
+                background: cIL > 0 ? "#f85525" : "unset",
+              }}>
+              {cIL}
+            </small>{" "}
+            {/* Cart Count */}
+            {openCart && (
+              <div className="cart-modal" onClick={(e) => e.stopPropagation()}>
+                <p>Cart Modal</p>
+              </div>
+            )}
+          </div>
+          <div className="screenmode-wrapper" onClick={handleScreenMode}>
+            {screenMode === "light" ? (
+              <i className="bi bi-moon"></i>
+            ) : (
+              <i className="bi bi-brightness-high"></i>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
