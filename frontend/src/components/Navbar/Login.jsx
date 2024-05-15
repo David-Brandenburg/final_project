71c38e3f65d8ebcc706b44e64d6ddff9c77cb1aa
win/ubuntu <d-brandenburg@hotmail.de>
2024-05-15 11:44:48 +0200

    login forntend broken

--- frontend/src/components/Navbar/Login.jsx
+++ frontend/src/components/Navbar/Login.jsx
@@ -1,6 +1,67 @@
-import React from "react";
+import { useState } from "react";
+import { toast } from "react-toastify";
 import "../../styles/modals.scss";
+
 const Login = () => {
+  const [formData, setFormData] = useState(new FormData());
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]{2,3}$/;
+    return re.test(email);
+  };
+
+  const validatePassword = (password) => {
+    const re =
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?])[a-zA-Z\d!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]{8,}$/;
+    return re.test(password);
+  };
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    const newFormData = new FormData(formData); // Copy existing FormData
+    newFormData.append(name, files ? files[0] : value);
+    setFormData(newFormData);
+  };
+
+  const appendData = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData(e.target);
+
+      // Validiere die E-Mail-Adresse
+      if (!validateEmail(formData.get("email"))) {
+        toast("Invalid email format!");
+        return;
+      }
+
+      // Validiere das Password
+      if (!validatePassword(formData.get("password"))) {
+        toast(
+          "Password must be at least 8 characters long and contain at least one number, one lowercase letter, one uppercase letter, and one special character (!@#$%^&*()_-=[]{};':\"|,.<>/?)."
+        );
+        return;
+      }
+
+      const resp = await fetch("http://localhost:3001/accounts/create", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!resp.ok) {
+        const data = await resp.json();
+        toast(data.message);
+      } else {
+        const data = await resp.json();
+        console.log(data);
+        toast(data.message);
+      }
+
+      // e.target.reset();
+    } catch (error) {
+      console.error("Error appending data to server", error);
+    }
+  };
+
   return (
     <div className="login-modal" onClick={(e) => e.stopPropagation()}>
       <form
@@ -8,8 +69,8 @@ const Login = () => {
         action=""
         method="post"
         encType="multipart/form-data" // Hier enctype hinzufügen
-      >
-        <h1 className="">Login</h1>
+        onSubmit={appendData}>
+        <h1 className=" ">Sign Up</h1>
         <div className="">
           <svg
             viewBox="0 0 16 16"
@@ -17,15 +78,34 @@ const Login = () => {
             height="16"
             width="16"
             xmlns="http://www.w3.org/2000/svg"
-            className="self-center">
+            className="self-center text-white">
+            <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
+          </svg>
+          <input
+            type="text"
+            name="benutzername"
+            className="login-input"
+            placeholder="Username"
+            autoComplete="off"
+          />
+        </div>
+        <div className="">
+          <svg
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            height="16"
+            width="16"
+            xmlns="http://www.w3.org/2000/svg"
+            className=" ">
             <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
           </svg>
           <input
             type="text"
             name="email"
             className="login-input"
-            placeholder="Email"
+            placeholder="@ zeichen & .de, .com, etc."
             autoComplete="off"
+            onChange={handleChange}
           />
         </div>
         <div className="">
@@ -40,18 +120,19 @@ const Login = () => {
           </svg>
           <input
             type="password"
-            className="login-input"
             name="password"
-            placeholder="Password"
+            className="login-input"
+            placeholder="min. 8 Zeichen, 1 A-Z, 1 a-z, 1 Zahl"
+            onChange={handleChange}
           />
         </div>
+
         <div className="login-btn-wrapper">
           <button className="login-button" type="submit">
-            Login
+            Sign Up!
           </button>
-          <button className="login-button">Sign Up</button>
+          <button className="login-button">Log In!</button>
         </div>
-        <button className="forgot-button">Forgot Password</button>
       </form>
     </div>
   );
