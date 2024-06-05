eec5a969403a25e0e36c74970f485da660475ecb
win/ubuntu <d-brandenburg@hotmail.de>
2024-06-05 14:22:18 +0200

    Beliebte Spiele liste zu Homepage hinzugefügt.

--- frontend/src/components/Navbar/GameModal.jsx
+++ frontend/src/components/Navbar/GameModal.jsx
@@ -1,74 +1,81 @@
-import { NavLink } from 'react-router-dom';
-import '../../styles/modals.scss';
-import { useContext, useEffect, useState } from 'react';
-import { ModalContext } from '../../contexts/ModalContext';
-import GameModalCardContainer from './GameModalCardContainer';
-import slugify from 'slugify';
+import { NavLink } from "react-router-dom";
+import "../../styles/modals.scss";
+import { useContext, useEffect, useState } from "react";
+import { ModalContext } from "../../contexts/ModalContext";
+import GameModalCardContainer from "./GameModalCardContainer";
+import slugify from "slugify";
 
 const GameModal = () => {
-	const [genre, setGenre] = useState('')
+  const [genre, setGenre] = useState("");
 
-	const { setOpenGameModal, setOpenModalBlocker } = useContext(ModalContext)
+  const { setOpenGameModal, setOpenModalBlocker } = useContext(ModalContext);
 
   useEffect(() => {
-    const gamesGenre = document.querySelectorAll('.games-modal-link');
+    const gamesGenre = document.querySelectorAll(".games-modal-link");
 
     const handleMouseEnter = (e) => {
       const currentGenre = e.target;
       setGenre(slugify(currentGenre.innerText.toLowerCase()));
-      
+
       gamesGenre.forEach((genre) => {
         if (genre !== currentGenre) {
-          genre.classList.remove('active');
+          genre.classList.remove("active");
         }
       });
 
-      currentGenre.classList.add('active');
+      currentGenre.classList.add("active");
     };
 
     gamesGenre.forEach((genre) => {
-      genre.addEventListener('mouseenter', handleMouseEnter);
+      genre.addEventListener("mouseenter", handleMouseEnter);
     });
 
     return () => {
       gamesGenre.forEach((genre) => {
-        genre.removeEventListener('mouseenter', handleMouseEnter);
+        genre.removeEventListener("mouseenter", handleMouseEnter);
       });
     };
   }, [genre]);
 
-    return (
-      <div className='games-modal' onClick={((e) => e.stopPropagation())}>
-        <div className='games-genre-list'>
-          <p className='games-modal-link' >Neu erschienen</p>
-          <p className='games-modal-link' >Beliebte Titel</p>
-          <p className='games-modal-link' >Angebote</p>
-					<hr style={{borderBottom: "1px solid #fff"}}/>
-          <p className='games-modal-link' >Abenteuer</p>
-          <p className='games-modal-link' >Action</p>
-          <p className='games-modal-link' >Shooter</p>
-          <p className='games-modal-link' >Rollenspiel</p>
-          <p className='games-modal-link' >Strategie</p>
-          <p className='games-modal-link' >Fantasy</p>
-          <p className='games-modal-link' >Science-Fiction</p>
-        </div>
-        {genre && <div className='games-content'>
-					<GameModalCardContainer genre={genre} />
-					<div className=''>
-						<NavLink to={`/games/genres/${genre.toLowerCase()}`} className='genre-link' onClick={((e) => {setOpenGameModal(false); setOpenModalBlocker(false);})}>
-							{
-								genre === 'beliebte-titel'
-								? 'Zu allen beliebten Titeln'
-								: genre === 'neu-erschienen'
-								? 'Zu allen Neuerscheinungen'
-								: genre === 'angebote'
-								? 'Zu allen Angeboten'
-								: `Zu allen ${slugify(genre)}-Spielen`}
-						</NavLink>
-					</div>
-        </div>}
+  return (
+    <div className="games-modal" onClick={(e) => e.stopPropagation()}>
+      <div className="games-genre-list">
+        <p className="games-modal-link">Neu erschienen</p>
+        <p className="games-modal-link">Beliebte Titel</p>
+        <p className="games-modal-link">Angebote</p>
+        <hr style={{ borderBottom: "1px solid #fff" }} />
+        <p className="games-modal-link">Abenteuer</p>
+        <p className="games-modal-link">Action</p>
+        <p className="games-modal-link">Shooter</p>
+        <p className="games-modal-link">Rollenspiel</p>
+        <p className="games-modal-link">Strategie</p>
+        <p className="games-modal-link">Fantasy</p>
+        <p className="games-modal-link">Science-Fiction</p>
       </div>
-    )
-}
+      {genre && (
+        <div className="games-content">
+          <GameModalCardContainer genre={genre} />
+          <div className="">
+            <NavLink
+              to={`/games/genres/${genre.toLowerCase()}`}
+              className="genre-link"
+              onClick={(e) => {
+                setOpenGameModal(false);
+                setOpenModalBlocker(false);
+              }}>
+              {genre === "beliebte-titel"
+                ? "Zu allen beliebten Titeln"
+                : genre === "neu-erschienen"
+                ? "Zu allen Neuerscheinungen"
+                : genre === "angebote"
+                ? "Zu allen Angeboten"
+                : `Zu allen ${slugify(genre)}-Spielen`}
+            </NavLink>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default GameModal;
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import HeroSection from "../../components/HomePage/HeroSection";
+import BeliebteGamesListe from "../../components/HomePage/BeliebteGamesListe";
+import "./homepage.scss";
 
 const HomePage = () => {
   return (
     <div className="main-wrapper">
       <HeroSection />
+      <div className="middle-section">
+        <div className="left">
+          <BeliebteGamesListe />
+        </div>
+        <div className="right">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Exercitationem odit eum excepturi provident nisi cum iure doloribus
+            voluptas fuga eaque?
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
