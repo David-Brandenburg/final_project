6513afac9991e1a468c58ea94dbc5ae616cc6770
win/ubuntu <d-brandenburg@hotmail.de>
2024-06-10 14:06:39 +0200

    Neu erscheinung liste fertig

---
+++ frontend/src/components/HomePage/NeueErschinenGamesListe.jsx
@@ -0,0 +1,80 @@
+import { useState, useEffect } from "react";
+import PageSubtitle from "../PageSubtitle/PageSubtitle.jsx";
+import "./NeueErschinenGamesListe.scss";
+import BeliebteGamesModal from "./BeliebteGamesModal.jsx";
+
+const NeueErschinenGamesListe = () => {
+  const [games, setGames] = useState([]);
+  const [hoveredGame, setHoveredGame] = useState(null);
+
+  const fetchGames = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3001/games/genres/neu-erschienen`
+      );
+      const data = await response.json();
+      setGames(data);
+      if (response.ok) {
+        console.log("Games fetched successfully");
+      }
+    } catch (error) {
+      console.error("Failed to fetch games:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
+  const handleMouseEnter = (game) => {
+    setHoveredGame(game);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredGame(null);
+  };
+
+  return (
+    <>
+      <PageSubtitle title="Neu Erscheinung" icon="dropbox" Nav={true} />
+      {games.length > 0 ? (
+        <div className="beliebte-spiele-wrapper">
+          <div className="row">
+            {games.slice(0, 5).map((game, index) => (
+              <div className="col-md-4" key={game.title + index}>
+                <div
+                  className="game-card"
+                  onMouseEnter={() => handleMouseEnter(game)}
+                  onMouseLeave={handleMouseLeave}>
+                  {hoveredGame === game && <BeliebteGamesModal game={game} />}
+                  <div className="game-card-thumbnail-wrapper">
+                    <img
+                      src={game.thumbnail}
+                      alt={game.title}
+                      className="game-card-thumbnail"
+                    />
+                  </div>
+                  <div className="game-card-content">
+                    <h3 className="game-card-title">{game.title}</h3>
+                    <p className="game-card-price">{game.price} €</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h3>Error</h3>
+              <p>Something went wrong</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default NeueErschinenGamesListe;
--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import HeroSection from "../../components/HomePage/HeroSection";
 import BeliebteGamesListe from "../../components/HomePage/BeliebteGamesListe";
 import "./homepage.scss";
+import NeueErschinenGamesListe from "../../components/HomePage/NeueErschinenGamesListe";
 
 const HomePage = () => {
   return (
@@ -12,11 +13,7 @@ const HomePage = () => {
           <BeliebteGamesListe />
         </div>
         <div className="right">
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Exercitationem odit eum excepturi provident nisi cum iure doloribus
-            voluptas fuga eaque?
-          </p>
+          <NeueErschinenGamesListe />
         </div>
       </div>
     </div>
