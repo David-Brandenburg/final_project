80bbf3faae439bcbef4177f19044304598aa9246
win/ubuntu <d-brandenburg@hotmail.de>
2024-06-13 10:30:53 +0200

    npm audit ausgeführt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -52,6 +52,7 @@ function App() {
       setOpenGameModal(false);
     }
   };
+
   return (
     <>
       <Navbar
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,22 +8,25 @@ import { ScreenModeContextProvider } from "./contexts/ScreenModeContext.js";
 import { LogginContextProvider } from "./contexts/LogginContext.js";
 import { AddtoCardContextProvider } from "./contexts/AddtoCardContext.js";
 import { LanguageProvider } from "./contexts/LanguageContext.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <LanguageProvider>
-    <AddtoCardContextProvider>
-      <LogginContextProvider>
-        <ScreenModeContextProvider>
-          <ModalContextProvider>
-            <Router>
-              <React.StrictMode>
-                <App />
-              </React.StrictMode>
-            </Router>
-          </ModalContextProvider>
-        </ScreenModeContextProvider>
-      </LogginContextProvider>
-    </AddtoCardContextProvider>
-  </LanguageProvider>
+  <GoogleOAuthProvider clientId="855300264733-7elgo4lffpu6mqknp4oju0n60elpa6id.apps.googleusercontent.com">
+    <LanguageProvider>
+      <AddtoCardContextProvider>
+        <LogginContextProvider>
+          <ScreenModeContextProvider>
+            <ModalContextProvider>
+              <Router>
+                <React.StrictMode>
+                  <App />
+                </React.StrictMode>
+              </Router>
+            </ModalContextProvider>
+          </ScreenModeContextProvider>
+        </LogginContextProvider>
+      </AddtoCardContextProvider>
+    </LanguageProvider>
+  </GoogleOAuthProvider>
 );
