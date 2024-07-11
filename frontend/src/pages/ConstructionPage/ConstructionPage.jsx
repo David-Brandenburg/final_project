669f023598823b170722200b71baeda2737fa05b
RealMahu <dannyboehm93@gmail.com>
2024-07-11 07:31:16 +0200

    construction page, fix navbar

--- frontend/src/pages/ConstructionPage/ConstructionPage.jsx
+++ frontend/src/pages/ConstructionPage/ConstructionPage.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './errorpage.scss'
+import './constructionpage.scss'
 import { useLanguage } from '../../contexts/LanguageContext.js';
 
-const ErrorPage = () => {
+const ConstructionPage = () => {
 	const { language } = useLanguage();
 
 	return (
-		<div className='main-wrapper error-wrapper'>
+		<div className='main-wrapper construction-wrapper'>
 			<h2>404</h2>
-			<p>{language === 'en' ? 'Page not found!' : 'Seite nicht gefunden!'}</p>
+			<p>{language === 'en' ? 'Page under construction!' : 'Seite befindet sich im Aufbau!'}</p>
 			<Link className='redirect-link' to='/'>{language === 'en' ? 'Back to Home' : 'Zurück zur Startseite'}</Link>
 		</div>
 	)
 }
 
-export default ErrorPage
+export default ConstructionPage
